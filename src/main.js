8bd9b3ca5bef900adb49a55efa1b1dd797ee8424
Manav Misra <manavm@visionwebsoft.com>
2024-11-25 19:52:06 -0600

    Add in-class codes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-// TODO: Do what must be done
+import { countDisplayRef, decrementRef, incrementRef, resetRef } from "./lib";
+
+let count = 0;
+
+function setCount(newCount) {
+  count = newCount;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  countDisplayRef.textContent = count;
+}
+
+incrementRef.addEventListener("click", () => {
+  setCount(count + 1);
+});
+
+decrementRef.addEventListener("click", () => {
+  setCount(count - 1);
+});
+
+resetRef.addEventListener("click", () => {
+  setCount(0);
+});
+
+updateDisplay();
