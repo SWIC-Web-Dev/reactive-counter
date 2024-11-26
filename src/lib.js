e9545eb7c08daa5ffb16c5b4a3bbe5a70d7488c5
Manav Misra <manavm@visionwebsoft.com>
2024-11-25 22:08:15 -0600

    Add in-class work

--- src/lib.js
+++
@@ -1,4 +0,0 @@
-export const countDisplayRef = document.getElementById("count-display");
-export const decrementRef = document.getElementById("decrement");
-export const incrementRef = document.getElementById("increment");
-export const resetRef = document.getElementById("reset");
